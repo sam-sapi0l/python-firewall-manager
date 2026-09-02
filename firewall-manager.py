dafcf4c6c0a32ffb63466f2bd5b74811b9c2b5fe
Gabriel Dallemole <143417447+sam-sapi0l@users.noreply.github.com>
2026-09-02 16:00:45 -0300

    Repo Adjustments

--- firewall-manager.py
+++ firewall-manager.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 
 from firewall_manager.menu import menu_options
@@ -12,19 +11,4 @@ def main():
     menu_options()
 
 if __name__ == "__main__":
-=======
-import sys
-
-from firewall_manager.menu import menu_options
-from firewall_manager.commands import check_root
-
-
-def main():
-    if not check_root():
-        sys.exit(1)
-
     menu_options()
-
-if __name__ == "__main__":
->>>>>>> 5b5421398db67d5734fc71484739817a178ba918
-    main()
