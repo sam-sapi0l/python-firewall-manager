cafbd749ddbabf224d471fd3a2aa4d88dddb3dcc
Gabriel Dallemole <143417447+sam-sapi0l@users.noreply.github.com>
2026-09-02 05:42:58 +0000

    Refactoring

---
+++ firewall_manager/menu.py
@@ -0,0 +1,42 @@
+from firewall_manager.rules import (
+    list_rules,
+    create_rule,
+    update_rule,
+    delete_rule,
+)
+from firewall_manager.backup import (
+    backup_rules,
+    restore_rules,
+)
+
+
+def menu_options():
+    while True:
+        print("\n===== IPTABLES MANAGER =====")
+        print("1 - List rules")
+        print("2 - Create rule")
+        print("3 - Update rule")
+        print("4 - Delete rule")
+        print("5 - Backup rules")
+        print("6 - Restore rules")
+        print("0 - Exit")
+
+        option = input("Option: ")
+
+        if option == "1":
+            list_rules()
+        elif option == "2":
+            create_rule()
+        elif option == "3":
+            update_rule()
+        elif option == "4":
+            delete_rule()
+        elif option == "5":
+            backup_rules()
+        elif option == "6":
+            restore_rules()
+        elif option == "0":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid option.")
